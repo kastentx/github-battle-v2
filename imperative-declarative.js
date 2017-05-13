990faa2c63a0b86e6a986bed009d6c7d22c37b98
Nick Kasten <kastencode@gmail.com>
2017-05-13 14:23:21 -0500

    adding examples

--- imperative-declarative.js
+++ imperative-declarative.js
@@ -1,3 +1,27 @@
-const double = inputArray => inputArray.map(e =>  e * 2)
+// DECLARATIVE
+const double = arr => arr.map(e =>  e * 2);
 
-console.log(double([1,2,3]));
+const add = arr => arr.reduce((a, b) => a + b);
+
+
+// IMPERATIVE
+function iDouble(arr) {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+    results.push(arr[i] * 2);
+  }
+  return results;
+}
+
+function iAdd(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
+}
+
+let sampleArr = [1, 2, 3];
+
+console.log(iDouble(sampleArr));
+console.log(add(sampleArr));
